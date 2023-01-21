8f63d5212f2eac4d5ddf7adf429f47e81a629c5d
Jerome Parramore (dev) <jerome.parramore@gmail.com>
2023-01-21 14:44:10 +0000

    add more test cases

--- tests/test_detector_credit_card.py
+++ tests/test_detector_credit_card.py
@@ -1,4 +1,5 @@
 import unittest
+import scrubadub
 
 from base import BaseTestCase
 
@@ -99,3 +100,54 @@ class CreditCardTestCase(unittest.TestCase, BaseTestCase):
         AFTER:  My credit card is {{CREDIT_CARD}}.
         """
         self.compare_before_after()
+
+    def test_beginning(self):
+        """
+        BEFORE: 4916370874154364 is my credit card.
+        AFTER:  {{CREDIT_CARD}} is my credit card.
+        """
+        self.compare_before_after()
+
+    def test_enclosed(self):
+        """
+        BEFORE: "4024007142038750" is my credit card.
+        AFTER:  "{{CREDIT_CARD}}" is my credit card.
+        """
+        self.compare_before_after()
+
+    def test_enclosed2(self):
+        """
+        BEFORE: (4024007142038750 is my credit card)
+        AFTER:  ({{CREDIT_CARD}}" is my credit card)
+        """
+        self.compare_before_after()
+
+    def test_enclosed3(self):
+        """
+        BEFORE: my number (4024007142038750) is safe
+        AFTER:  my number ({{CREDIT_CARD}}) is safe
+        """
+        self.compare_before_after()
+
+    def test_false_positives(self):
+        """test a simple matching"""
+
+        to_test = [
+            # positive assertions
+            ("4111111111111111", True),
+            ("4024007142038750", True),
+            # negative assertions
+            ("41111111111111119", False),
+            ("95130-64212", False),
+        ]
+
+        test_str = 'my credit card number is {}'
+        detector = scrubadub.detectors.CreditCardDetector(locale='en_GB')
+
+        for number, result in to_test:
+            matches = list(detector.iter_filth(test_str.format(number)))
+            if result:
+                self.assertEquals(len(matches), 1)
+                self.assertEquals(matches[0].text, number)
+            else:
+                self.assertEquals(matches, [])
